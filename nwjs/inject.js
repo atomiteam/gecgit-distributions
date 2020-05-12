d8ce522fa8e192c9ed92aaea9c000089526a4b3c
sami <samialtundag@gmail.com>
2020-05-12 04:46:55 +0300

    Local storage added

--- nwjs/inject.js
+++ nwjs/inject.js
@@ -1,91 +1,109 @@
-const { exec } = nw.require('child_process');
+const {exec} = nw.require('child_process');
 const fs = nw.require('fs');
-// Default Gecgit plugins
 
 // Shell plugin allows executing notes in terminal
-const shell = {
-        execute: function(args) {
-            this._execute(args.code.split('\n'), args.out);
-        },
-        _execute: function(commands, out){
-            const command = commands.shift();
-            const _ = this;
-            exec(command, (error, stdout, stderr) => {
-              out.log(`Executing\n${command}`);
-              if (error) {
+class Shell{
+    execute(args) {
+        this._execute(args.code.split('\n'), args.out);
+    }
+    _execute(commands, out) {
+        const command = commands.shift();
+        const _ = this;
+        exec(command, (error, stdout, stderr) => {
+            out.log(`Executing\n${command}`);
+            if (error) {
                 out.error(error);
                 return;
-              }
-              if(stdout) out.log(stdout);
-              if(stderr) out.error(stderr);
-              if(commands.length) _._execute(commands, out)
-            });
-        }
-     }
-
-
-
-// Local storage plugin
-// If enabled, authentication and cloud storage features of
-// Gecgit are disabled.
-// The user opens the application without login and notes
-// are stored in local disk.
-const storage  = {
-    read(){
-        try{
-            return JSON.parse(fs.readFileSync('store.json'));  
-        }catch(e){
-            return {};
-        }
-    },
-    write(items){
-        fs.writeFileSync('store.json', JSON.stringify(items))
-    },
-    list(args){
-        console.log(`list ${JSON.stringify(args)}`);
-        const _ = this;
-        return new Promise(function(accept){
-            try{
-                const items = Object.values(_.read());
-                const filtered = args.q ? items.filter( i=> i.content.contains(args.q)) : items;
-                accept({data: filtered});
-            }catch(e){
-                accept({data: []});
             }
-        });
-    },
-    create(post){
-        console.log(`create ${JSON.stringify(post)}`);
-        post.id = `id:${new Date().getTime()}`;
-        return this.update(post);
-    },
-    update(post){
-        console.log(`update ${JSON.stringify(post)}`);
-        const _ = this;
-        return new Promise(function(accept){
-            const items = _.read();
-            items[post.id] = post;
-            _.write(items);
-            accept({data: post});
-        });
-    },
-    remove(post){
-        console.log(`remove ${JSON.stringify(post)}`);
-        const _ = this;
-        return new Promise(function(accept){
-            const items = _.read();
-            delete items[post.id];
-            _.write(items);
-            accept({data: post});
+            if (stdout) out.log(stdout);
+            if (stderr) out.error(stderr);
+            if (commands.length) _._execute(commands, out)
         });
     }
 }
+
+// Minimal local storage implementation of Storage interface
+const UpdatedAtComparator = function(a, b){
+    return a.updatedAt - b.updatedAt;
+}
+
+class Storage {
+    async list(args) {
+        const items = this._read(args);
+        return {
+            data: Object.values(items).filter(i => {
+                if (args.q) {
+                    return i.content.includes(args.q);
+                } else {
+                    return true;
+                }
+            }).sort(UpdatedAtComparator)
+        };
+    }
+    async create(post) {
+        const posts = this._read();
+        post.updatedAt = new Date();
+        post.id = `id:${new Date().toLocaleString()}`;
+        posts[post.id] = post;
+        this._write(posts);
+        return {
+            data: post
+        };
+    }
+    async remove(post) {
+        const posts = this._read();
+        delete posts[post.id];
+        this._write(posts);
+        return {
+            data: post
+        };
+    }
+    async update(post) {
+        const posts = this._read();
+        posts[post.id] = post;
+        post.updatedAt = new Date();
+        this._write(posts);
+        return {
+            data: post
+        };
+    }
+    _read() {
+        try {
+            return JSON.parse(fs.readFileSync('store.json'));
+        } catch (e) {
+            return {};
+        }
+    }
+    _write(items) {
+        fs.writeFileSync('store.json', JSON.stringify(items))
+    }
+}
+
+// Minimal no authentication implementation of Authentication interface
+class Authentication{
+    get token(){
+        return '';
+    }
+    get local(){
+        return {};
+    }
+    isAuthenticated() {
+        return true;
+    }
+    authenticate() {
+        window.location = '/posts';
+    }
+}
+
+const shell = new Shell(), 
+    authentication = new Authentication(), 
+    storage = new Storage();
 
 window.gecgit = {
     plugins: {
-        shell: shell,
-        // Uncomment below line if you want local storage
-        // storage: storage
+        shell,
+        // Uncomment below line if local storage is desired
+        //storage,
+        //authentication
     }
 }
-
